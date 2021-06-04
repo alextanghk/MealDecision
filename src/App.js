2316d5e487b97f67b599197a25c582e68fb3058a
alextangweb <alextang1030@gmail.com>
2021-06-04 15:31:39 +0800

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import _ from "lodash";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const locations = [
+  "薄扶林","石塘咀","西營盤","上環","中環","金鐘","灣仔","銅鑼灣","天后","炮台山","鰂魚涌","北角","太古","杏花村","柴灣","西灣河","筲箕灣","黃竹坑","利東","海怡","深水埗","油麻地","旺角","太子","尖沙咀","紅磡","何文田","九龍塘","九龍灣","牛頭角","樂富","佐敦","九龍城","黃大仙","石硤尾","鑽石山","彩虹","油塘","調景嶺","觀塘","葵芳","葵涌","葵興","大窩口","荔景","荔枝角","美孚","長沙灣","荃灣","屯門","元朗","朗屏","新界北","大埔","太和","錦上路","天水圍","大圍","粉嶺","青衣","東涌","沙田","火炭","上水","屯門中心","友愛","馬鞍山","第一城","石門","大水坑","兆康","西貢","將軍澳","康城","坑口","寶琳","長洲","平洲","大嶼山"
+];
+
+const load = (callback) => {
+  window.gapi.client.load("sheets", "v4", () => {
+    window.gapi.client.sheets.spreadsheets.values
+      .get({
+        spreadsheetId: process.env.REACT_APP_SPREADSHEET_ID,
+        range: `${process.env.REACT_APP_SHEET_ID}!A2:D`
+      })
+      .then(
+        response => {
+          const data = response.result.values;
+          const items = data.map(item => ({
+            name: item[0],
+            location: item[1],
+            address: item[2],
+            price_range: item[3],
+          })) || [];
+          callback({
+            items
+          });
+        },
+        response => {
+          callback(false, response.result.error);
+        }
+      );
+  });
 }
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      result: null,
+      selected: "",
+      loading: true
+    }
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+    window.gapi.load("client", this.initClient);
+  }
+
+  initClient = () => {
+    console.log("here");
+    window.gapi.client.init({
+      apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+      discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"]
+    }).then(()=>{
+      load(this.onLoad);
+    })
+  }
+
+  onLoad = (data,error) => {
+    if (data) {
+      this.setState(prevState => ({
+        items: data.items
+      }))
+    }
+  }
+
+  onRandom = () => {
+    const location = this.state.selected;
+    console.log(this.state.items);
+    const items = (location != "" && location !== undefined) ? _.filter(this.state.items, {location: location}) : this.state.items;
+    console.log(items);
+    const random = Math.floor(Math.random()*items.length);
+    const result = _.get(items,`[${random}]`,null);
+    console.log(result);
+    this.setState(prevState=>({
+      ...prevState,
+      result:result
+    }))
+  }
+
+  render() {
+    const randomResult = this.state.result;
+    return (
+      <div className="App">
+        <select onChange={(e)=>{
+          this.setState(prevState=>({
+            ...prevState,
+            selected: e.target.value
+          }))
+          
+        }}>
+          <option>-- Please Select --</option>
+          { locations.map((v)=> { return(<option value={v}>{v}</option>); }) }
+        </select><button onClick={(e)=>{this.onRandom()}}>Random</button><br/>
+        <label>{`Name: ${_.get(randomResult,"name","")}`}</label><br/>
+        <label>{`Address: ${_.get(randomResult,"address","")}`}</label><br/>
+        <label>{`Price range: ${_.get(randomResult,"price_range","")}`}</label><br/>
+      </div>
+    ); 
+  }
+}

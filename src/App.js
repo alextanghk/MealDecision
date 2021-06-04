d6a8591056cc0fcf74a7540b00265d8d0b5eae2a
alextangweb <alextang1030@gmail.com>
2021-06-04 17:12:53 +0800

    style first draft

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import _ from "lodash";
+import './styles.scss';
+import { Grid, Card, CardHeader,CardContent } from "@material-ui/core";
 
 const locations = [
   "薄扶林","石塘咀","西營盤","上環","中環","金鐘","灣仔","銅鑼灣","天后","炮台山","鰂魚涌","北角","太古","杏花村","柴灣","西灣河","筲箕灣","黃竹坑","利東","海怡","深水埗","油麻地","旺角","太子","尖沙咀","紅磡","何文田","九龍塘","九龍灣","牛頭角","樂富","佐敦","九龍城","黃大仙","石硤尾","鑽石山","彩虹","油塘","調景嶺","觀塘","葵芳","葵涌","葵興","大窩口","荔景","荔枝角","美孚","長沙灣","荃灣","屯門","元朗","朗屏","新界北","大埔","太和","錦上路","天水圍","大圍","粉嶺","青衣","東涌","沙田","火炭","上水","屯門中心","友愛","馬鞍山","第一城","石門","大水坑","兆康","西貢","將軍澳","康城","坑口","寶琳","長洲","平洲","大嶼山"
@@ -88,19 +90,59 @@ export default class App extends Component {
     const randomResult = this.state.result;
     return (
       <div className="App">
-        <select onChange={(e)=>{
-          this.setState(prevState=>({
-            ...prevState,
-            selected: e.target.value
-          }))
-          
-        }}>
-          <option>-- Please Select --</option>
-          { locations.map((v)=> { return(<option value={v}>{v}</option>); }) }
-        </select><button onClick={(e)=>{this.onRandom()}}>Random</button><br/>
-        <label>{`Name: ${_.get(randomResult,"name","")}`}</label><br/>
-        <label>{`Address: ${_.get(randomResult,"address","")}`}</label><br/>
-        <label>{`Price range: ${_.get(randomResult,"price_range","")}`}</label><br/>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item md={4} xs={8}>
+            <Card>
+              <CardContent>
+                <Grid
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Grid item xs={4}>
+                    <Grid
+                      container
+                      direction="row"
+                    >
+                      <Grid item xs={12}>搵食地區 (Location):</Grid>
+                      <Grid item xs={12}>
+                        <select onChange={(e)=>{
+                          this.setState(prevState=>({
+                            ...prevState,
+                            selected: e.target.value
+                          }))
+                          
+                        }}>
+                          <option>-- Please Select --</option>
+                          { locations.map((v)=> { return(<option value={v}>{v}</option>); }) }
+                        </select>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <button onClick={(e)=>{this.onRandom()}}>Random</button>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Grid
+                      container
+                      direction="row"
+                    >
+                      <Grid item xs={4}>餐廳 (Restaurant):</Grid><Grid item xs={8}>{`${_.get(randomResult,"name","無紀錄")}`}</Grid>
+                      <Grid item xs={4}>地址 (Address):</Grid><Grid item xs={8}>{`${_.get(randomResult,"address","無紀錄")}`}</Grid>
+                      <Grid item xs={4}>價格範圍 / 每人 (Price Range / Per Person):</Grid><Grid item xs={8}>{`${_.get(randomResult,"price_range","無紀錄")}`}</Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
       </div>
     ); 
   }

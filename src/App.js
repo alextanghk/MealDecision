cf25f8f3d03157f8d9b6147cb4cc12e05f693aaa
alextangweb <alextang1030@gmail.com>
2021-06-04 19:38:18 +0800

    read locations from sheet instead of var

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import _ from "lodash";
 import './styles.scss';
 import { Grid, Card,CardContent } from "@material-ui/core";
 
-const locations = [
-  "薄扶林","石塘咀","西營盤","上環","中環","金鐘","灣仔","銅鑼灣","天后","炮台山","鰂魚涌","北角","太古","杏花村","柴灣","西灣河","筲箕灣","黃竹坑","利東","海怡","深水埗","油麻地","旺角","太子","尖沙咀","紅磡","何文田","九龍塘","九龍灣","牛頭角","樂富","佐敦","九龍城","黃大仙","石硤尾","鑽石山","彩虹","油塘","調景嶺","觀塘","葵芳","葵涌","葵興","大窩口","荔景","荔枝角","美孚","長沙灣","荃灣","屯門","元朗","朗屏","新界北","大埔","太和","錦上路","天水圍","大圍","粉嶺","青衣","東涌","沙田","火炭","上水","屯門中心","友愛","馬鞍山","第一城","石門","大水坑","兆康","西貢","將軍澳","康城","坑口","寶琳","長洲","平洲","大嶼山"
-];
 
 const load = (callback) => {
   window.gapi.client.load("sheets", "v4", () => {
     window.gapi.client.sheets.spreadsheets.values
       .get({
         spreadsheetId: process.env.REACT_APP_SPREADSHEET_ID,
-        range: `${process.env.REACT_APP_SHEET_ID}!A2:D`
+        range: `${process.env.REACT_APP_SHEET_ID}!A2:E`
       })
       .then(
         response => {
@@ -23,6 +20,30 @@ const load = (callback) => {
             location: item[1],
             address: item[2],
             price_range: item[3],
+            type: item[4],
+          })) || [];
+          callback({
+            items
+          });
+        },
+        response => {
+          callback(false, response.result.error);
+        }
+      );
+  });
+}
+const loadLocations = (callback) => {
+  window.gapi.client.load("sheets", "v4", () => {
+    window.gapi.client.sheets.spreadsheets.values
+      .get({
+        spreadsheetId: process.env.REACT_APP_SPREADSHEET_ID,
+        range: `${process.env.REACT_APP_SHEET_LOC}!A2:B`
+      })
+      .then(
+        response => {
+          const data = response.result.values;
+          const items = data.map(item => ({
+            zh_name: item[0]
           })) || [];
           callback({
             items
@@ -40,6 +61,7 @@ export default class App extends Component {
     super(props);
     this.state = {
       items: [],
+      locations:[],
       result: null,
       selected: "",
       loading: true
@@ -55,12 +77,12 @@ export default class App extends Component {
   }
 
   initClient = () => {
-    console.log("here");
     window.gapi.client.init({
       apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
       discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"]
     }).then(()=>{
       load(this.onLoad);
+      loadLocations(this.onLoadLocation);
     })
   }
 
@@ -72,14 +94,19 @@ export default class App extends Component {
     }
   }
 
+  onLoadLocation = (data,error) => {
+    if (data) {
+      this.setState(prevState => ({
+        locations: data.items
+      }))
+    }
+  }
+
   onRandom = () => {
     const location = this.state.selected;
-    console.log(this.state.items);
     const items = (location !== "" && location !== undefined) ? _.filter(this.state.items, {location: location}) : this.state.items;
-    console.log(items);
     const random = Math.floor(Math.random()*items.length);
     const result = _.get(items,`[${random}]`,null);
-    console.log(result);
     this.setState(prevState=>({
       ...prevState,
       result:result
@@ -120,7 +147,7 @@ export default class App extends Component {
                           
                         }}>
                           <option>-- Please Select --</option>
-                          { locations.map((v)=> { return(<option value={v}>{v}</option>); }) }
+                          { (this.state.locations != null) && this.state.locations.map((v)=> { return(<option value={v.zh_name}>{v.zh_name}</option>); }) }
                         </select>
                       </Grid>
                       <Grid item xs={12}>
@@ -135,6 +162,7 @@ export default class App extends Component {
                     >
                       <Grid item xs={4}>餐廳 (Restaurant):</Grid><Grid item xs={8}>{`${_.get(randomResult,"name","無紀錄")}`}</Grid>
                       <Grid item xs={4}>地址 (Address):</Grid><Grid item xs={8}>{`${_.get(randomResult,"address","無紀錄")}`}</Grid>
+                      <Grid item xs={4}>種類 (Type):</Grid><Grid item xs={8}>{`${_.get(randomResult,"type","無紀錄")}`}</Grid>
                       <Grid item xs={4}>價格範圍 / 每人 (Price Range / Per Person):</Grid><Grid item xs={8}>{`${_.get(randomResult,"price_range","無紀錄")}`}</Grid>
                     </Grid>
                   </Grid>
